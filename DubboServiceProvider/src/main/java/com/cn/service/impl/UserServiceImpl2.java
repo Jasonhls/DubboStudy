3c3484f2353e39280bb7665cffb36b3d31b513ba
hels <1597445096@qq.com>
2021-03-04 18:37:49 +0800

    dubbo的超时，重试，灰度支持测试

--- DubboServiceProvider/src/main/java/com/cn/service/impl/UserServiceImpl2.java
+++ DubboServiceProvider/src/main/java/com/cn/service/impl/UserServiceImpl2.java
@@ -7,13 +7,14 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * @author admin
- */
-public class UserServiceImpl implements UserService {
-
+ * @description:
+ * @author: helisen
+ * @create: 2021-03-04 17:06
+ **/
+public class UserServiceImpl2 implements UserService {
     @Override
     public List<UserAddress> getUserAddressList(String userId) {
-        System.out.println("调用了服务提供方的getUserAddressList方法。。。版本01");
+        System.out.println("调用了服务提供方的getUserAddressList方法。。。版本02");
         UserAddress userAddress1 = new UserAddress(1, "北京市昌平区宏福科技园综合楼3层","1", "李老师", "010-12477555", "Y");
         UserAddress userAddress2 = new UserAddress(1, "深圳市宝安区西部硅谷大厦B座3层（深圳分校）","2", "王老师", "0755-89587458", "N");
         try {
