58b0c8221f94d7453c5c433639c5b9297f6f5958
hels <1597445096@qq.com>
2021-03-05 15:49:18 +0800

    测试设置dubbo容错策略

--- SpringBootDubboServiceConsumer/src/main/java/com/cn/service/impl/OrderServiceImpl.java
+++ SpringBootDubboServiceConsumer/src/main/java/com/cn/service/impl/OrderServiceImpl.java
@@ -19,12 +19,12 @@ public class OrderServiceImpl implements OrderService {
 
     /**
      * 消费服务，用这个注解，dubbo提供的注解
-     * 配置loadbalance属性，默认值为random，查看源码LoadBalance的实现类，即知道有几种策略，每种策略的name是什么
-     * 配置cluster属性，即集群容错配置，查看源码com.alibaba.dubbo.rpc.cluster.Cluster的实现类，默认是failover
-     * failover（默认值）：会去调用远程服务，如果失败，会重试两次，不知道为啥是两次？
-     * failfast：会去调用远程服务，如果失败，直接报错，不会进行重试
-     * failback：会去调用远程服务，如果失败，就返回为null，会进行重试，默认执行一次。
-     * failsafe：会去调用远程服务，如果失败，就返回null，不会执行默认的重试一次。
+     * 1.配置loadbalance属性，默认值为random，查看源码LoadBalance的实现类，即知道有几种策略，每种策略的name是什么
+     * 2.配置cluster属性，即集群容错配置，查看源码com.alibaba.dubbo.rpc.cluster.Cluster的实现类，默认是failover
+     *      failover（默认值）：会去调用远程服务，如果失败，会重试两次，不知道为啥是两次？
+     *      failfast：会去调用远程服务，如果失败，直接报错，不会进行重试
+     *      failback：会去调用远程服务，如果失败，就返回为null，会进行重试，默认执行一次。
+     *      failsafe：会去调用远程服务，如果失败，就返回null，不会执行默认的重试一次。
      *
      */
 //    @Autowired
