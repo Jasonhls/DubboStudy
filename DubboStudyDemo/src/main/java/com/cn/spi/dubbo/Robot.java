dbf6c86f0e2bd41bac1dde959265349e0593da59
hels <1597445096@qq.com>
2021-03-09 18:08:55 +0800

    java的SPI与dubbo的SPI以及dubbo的SPI Aop的学习

--- DubboStudyDemo/src/main/java/com/cn/spi/dubbo/Robot.java
+++ DubboStudyDemo/src/main/java/com/cn/spi/dubbo/Robot.java
@@ -6,7 +6,8 @@ import org.apache.dubbo.common.extension.SPI;
  * @description:
  * @author: helisen
  * @create: 2021-03-09 15:20
- * @SPI中的value表示默认的扩展类，如果没有配置value，就没有默认的扩展类
+ * @SPI中的value表示默认的扩展类，如果没有配置value，就没有默认的扩展类，
+ * 这里配置value为true也相当于没有配置默认扩展类，具体源码可以看dubbo的ExtensionLoader类的getDefaultExtension()方法
  **/
 @SPI(value = "bumblebee")
 public interface Robot {
