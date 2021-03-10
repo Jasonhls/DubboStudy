a3191b1fb43a54ef3aed29e9d150566ee21dbff2
hels <1597445096@qq.com>
2021-03-10 09:36:34 +0800

    测试方法

--- DubboStudyDemo/src/main/java/com/cn/spi/dubboadaptive/DubboAdaptiveTest.java
+++ DubboStudyDemo/src/main/java/com/cn/spi/dubboadaptive/DubboAdaptiveTest.java
@@ -2,6 +2,7 @@ package com.cn.spi.dubboadaptive;
 
 import org.apache.dubbo.common.URL;
 import org.apache.dubbo.common.extension.ExtensionLoader;
+import org.apache.dubbo.common.utils.StringUtils;
 import org.junit.Test;
 
 public class DubboAdaptiveTest {
@@ -35,6 +36,16 @@ public class DubboAdaptiveTest {
         URL url = URL.valueOf("test://localhost/test?myAdaptiveName=spring");
         //即使上面的url中myAdaptiveName=spring，但是DubboAdaptiveExtImpl类上配置了注解@Adaptive，因此会优先调用该类。
         System.out.println(adaptiveExt.echo("d", url));
+    }
+
+    @Test
+    public void testFour() {
+        Class type = AdaptiveExt.class;
+        // AdaptiveExt
+        System.out.println(type.getSimpleName());
+        String splitName = StringUtils.camelToSplitName(type.getSimpleName(), ".");
+        // adaptive.ext
+        System.out.println(splitName);
     }
 
 }
